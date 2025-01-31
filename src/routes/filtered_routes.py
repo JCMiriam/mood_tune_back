b613775517f1a980effdf19a005dbb26604f61ee
Miriam JC <myriamkey612@gmail.com>
2025-02-01 00:57:48 +0100

    add new endpoint

--- src/routes/filtered_routes.py
+++ src/routes/filtered_routes.py
@@ -15,3 +15,9 @@ def filtered_followed_artists():
     user_artists = request.json.get("artists", [])
     filtered_artists = check_artists_in_dataset(user_artists)
     return jsonify({"filtered_artists": filtered_artists})
+
+@filtered_bp.route("/filtered-favourite-tracks", methods=["POST"])
+def filtered_favourite_tracks():
+    user_tracks = request.json.get("tracks", [])
+    filtered_tracks = check_songs_in_dataset(user_tracks)
+    return jsonify({"filtered_tracks": filtered_tracks})
